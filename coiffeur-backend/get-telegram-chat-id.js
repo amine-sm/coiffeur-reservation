4da9537be3f9f8e7ea17f47e40cdbe1d64cbe9b5
mohammed el amine <aminesaidmansour73@gmail.com>
2026-05-13 21:36:06 +0100

    UPDATE

--- coiffeur-backend/get-telegram-chat-id.js
+++ coiffeur-backend/get-telegram-chat-id.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const TelegramBot = require("node-telegram-bot-api");
 
-const token = process.env.TELEGRAM_BOT_TOKEN;
+const token = process.envTELEGRAM_BOT_TOKEN ;
 
 if (!token) {
     console.error("❌ TELEGRAM_BOT_TOKEN manquant dans .env");
